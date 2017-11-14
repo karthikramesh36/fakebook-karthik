14651730180671908fb576adc50d3a16c28b9c21
karthik Ramesh <karthikramesh36@gmail.com>
2017-11-14 05:42:18 +0000

    14 commit

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ UPLOAD_FOLDER = '/home/ubuntu/workspace/flaskbook/static/images'
 STATIC_IMAGE_URL = 'images'
 AWS_BUCKET = 'fakebookbackend'
 AWS_CONTENT_URL = 'https://s3-us-west-2.amazonaws.com'
-MONGODB_HOST = 'ds159235.mlab.com/fakebook36'
-MONGODB_PORT = 59235
-MONGODB_USERNAME = 'fakebookadmin'
-MONGODB_PASSWORD = 'peace'
+MONGODB_SETTINGS = {
+    'db': 'fakebook36',
+    'host': 'mongodb://fakebookadmin:peace@ds159235.mlab.com:59235/fakebook36'
+}
