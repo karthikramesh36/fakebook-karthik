add9d66143d820cee7a720a374a4122b2d9a9da3
karthik Ramesh <karthikramesh36@gmail.com>
2017-11-14 04:44:52 +0000

    11 commit

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 SECRET_KEY = 'you-will-never-guess'
 DEBUG=True
 MONGODB_DB = 'fakebook36'
-MONGOLAB_URI = 'mongodb://<dbuser>:<dbpassword>@ds159235.mlab.com:59235/fakebook36'
+MONGOLAB_URI = 'mongodb://fakebookadmin:peace@ds159235.mlab.com:59235/fakebook36'
 HOSTNAME = 'http://flaskbook-karthikramesh36.c9users.io/'
 UPLOAD_FOLDER = '/home/ubuntu/workspace/flaskbook/static/images'
 STATIC_IMAGE_URL = 'images'
