7a9356efc68f79ca12201be48ad3ec778dc61068
karthik Ramesh <karthikramesh36@gmail.com>
2017-11-14 04:49:33 +0000

    12 commit

--- manage.py
+++ manage.py
@@ -5,20 +5,16 @@ from flask.ext.script import Manager, Server
 from application import create_app
 app = create_app()
 manager = Manager(app)
-"""
+
 manager.add_command("runserver", Server(
     use_debugger=True,
     use_reloader=True,
     host=os.getenv('IP', '0.0.0.0'),
     port=int(os.getenv('PORT', 5000)))
 )
-"""
 
-manager.add_command("runserver", Server(
-    use_debugger=True,
-    use_reloader=True,
-    host=os.getenv('MONGOLAB_URI'))
-)
+
+
 
 if __name__ == "__main__":
     manager.run()
