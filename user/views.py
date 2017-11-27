27e748a5261a077ab91470e639cdcc9aa1f74a31
karthik Ramesh <karthikramesh36@gmail.com>
2017-11-27 00:13:46 +0000

    My 24th commit

--- user/views.py
+++ user/views.py
@@ -11,6 +11,7 @@ from settings import UPLOAD_FOLDER
 from utilities.imaging import thumbnail_process
 from relationship.models import Relationship
 from user.decorators import login_required
+from feed.forms import FeedPostForm
 
 user_app = Blueprint('user_app', __name__)
 
@@ -112,6 +113,8 @@ def profile(username, page=1):
             friends = friends.paginate(page=page, per_page=3)
         else:
             friends = friends[:5]
+            
+        form = FeedPostForm()
         
         return render_template('user/profile.html', 
             user=user, 
@@ -120,6 +123,7 @@ def profile(username, page=1):
             friends=friends,
             friends_total=friends_total,
             friends_page=friends_page,
+            form=form
             )
     else:
         abort(404)
