e374ba02c73fe7e85cc0e6fb79121034aeda728f
karthikramesh36 <karthikramesh36@gmail.com>
2017-12-04 19:16:00 -0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,13 +2,13 @@ SECRET_KEY = 'you-will-never-guess'
 DEBUG=True
 MONGODB_DB = 'fakebook36'
 HOSTNAME = 'http://fakebook36.herokuapp.com/'
-MONGOLAB_URI = 'mongodb://fakebookadmin:peace@ds159235.mlab.com:59235/fakebook36'
+MONGOLAB_URI = 'mongodb://fakebookadmin:#####@ds159235.mlab.com:59235/fakebook36'
 UPLOAD_FOLDER = '/home/ubuntu/workspace/flaskbook/static/images'
 STATIC_IMAGE_URL = 'images'
 AWS_BUCKET = 'fakebookbackend'
 AWS_CONTENT_URL = 'https://s3-us-west-2.amazonaws.com'
 MONGODB_SETTINGS = {
     'db': 'fakebook36',
-    'host': 'mongodb://fakebookadmin:peace@ds159235.mlab.com:59235/fakebook36'
+    'host': 'mongodb://fakebookadmin:#####@ds159235.mlab.com:59235/fakebook36'
 }
 AWS_SEND_MAIL = True
